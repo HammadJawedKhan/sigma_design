c15a40423434f8697b96c8842656bbe64da949ec
Muhammad Hammad Khan <hjawed716@gmail.com>
2024-10-24 17:39:12 +0500

    second design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,7 @@
 import React from 'react'
+
 export default function page() {
   return (
-      <div> 
-<div>
-  hello
-  </div>        
-      
-      
-      
-    </div>
-  )
+    <div>Muhammad Hammad Khan is an aspiring web developer currently enrolled in the AI and Web 3.0 course under the Governor's initiative. With a keen interest in cutting-edge technologies like Next.js and Tailwind CSS, Hammad is focused on building responsive and dynamic web applications. His goal is to leverage the power of artificial intelligence and decentralized web technologies to create innovative solutions that make a real-world impact. As he continues to expand his skill set, Hammad is eager to collaborate, learn, and contribute to the growing field of technology.</div>
+)
 }
-
