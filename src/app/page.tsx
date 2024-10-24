8bcd3906801618552d5092b5cc81c83a05614d38
Muhammad Hammad Khan <hjawed716@gmail.com>
2024-10-24 18:25:14 +0500

    third design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
 import React from 'react'
-
+import Image from 'next/image'
+import img from "../../public/Rectangle 6.png"
+import img1 from "../../public/Group 13@2x.png"
 export default function page() {
   return (
-    <div>Muhammad Hammad Khan is an aspiring web developer currently enrolled in the AI and Web 3.0 course under the Governor's initiative. With a keen interest in cutting-edge technologies like Next.js and Tailwind CSS, Hammad is focused on building responsive and dynamic web applications. His goal is to leverage the power of artificial intelligence and decentralized web technologies to create innovative solutions that make a real-world impact. As he continues to expand his skill set, Hammad is eager to collaborate, learn, and contribute to the growing field of technology.</div>
+    <div>
+    <div className='flex flex-col lg:flex-row lg:ml-14 mb-4 mt-4'>
+        {/* Left Section */}
+        <div className="lg:w-1/2 p-4 lg:ml-0">
+          <div className="mt-44 lg:ml-20 ">
+            <h1 className='font-light text-3xl md:text-5xl text-gray-400 mt-5'>
+              Project
+            </h1>
+            <h2 className='font-extrabold text-3xl md:text-5xl mt-3'>
+              lorem
+            </h2>
+            
+          </div>
+        </div>
+
+        {/* Right Section */}
+        <div className=' lg:ml-12 lg:w-1/2 p-4 mr-20'>
+        <Image src={img} alt='' />  
+        </div>
+      </div> 
+       <div>
+        <Image src={img1} alt=''/>
+       </div>
+    </div>
 )
 }
